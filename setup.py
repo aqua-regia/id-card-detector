5f558cfcc7625f39836ec4a50f5158058dfdce79
hassan_ashraf_ <hassan@coinswitch.co>
2020-11-11 14:32:36 +0530

    changed version number and refactored to expose things as function

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="exceptiom",
-    version="1.3.0",
-    description="Make python exceptions fun",
+    name="id_card_extractor",
+    version="1.1.0",
+    description="Crop id card from an image using OpenCV",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/aqua-regia/exceptioms",
+    url="https://github.com/aqua-regia/id-card-detector",
     author="Syed Hassan Ashraf",
     author_email="hassanashraf8888@gmail.com",
     license="MIT",
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
-    packages=["exceptiom"],
+    packages=["id_card_extractor"],
     include_package_data=True,
     install_requires=[],
     entry_points={
